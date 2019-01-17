f230ee34963520f482b55854ef28c73150110b53
Guy-Vincent Jourdan <gvj@site.uottawa.ca>
2019-01-17 16:59:38 -0500

    Code from the first 2 lectures (on types)

---
+++ 02-Types/Wrapper.java
@@ -0,0 +1,12 @@
+public class Wrapper {
+	
+	public static void main(String[] args){
+		
+		// 1: normal declaration;
+		Integer i = new Integer(50);
+
+		// 2: auto boxing;
+		Integer j = 50;
+
+	}
+}
